f89387dcf43a0ac7b5fda3eee6b7b543eba13824
Ivann <ivann.machado@laplateforme.io>
2025-12-17 11:19:14 +0100

    user logic

---
+++ backend/src/controllers/user.controller.js
@@ -0,0 +1,19 @@
+/**
+ * @api {get} /api/profile Voir son profil
+ * @apiName GetProfile
+ * @apiGroup User
+ * @apiDescription Récupère les informations de l'utilisateur connecté à partir de son jeton JWT.
+ * * @apiHeader {String} Authorization Jeton JWT (Format: Bearer TOKEN).
+ *
+ * @apiSuccess {String} message Message de succès.
+ * @apiSuccess {Object} user Informations contenues dans le token (id, pseudo, email, role, etc.).
+ *
+ * @apiError (401) {String} error Token manquant ou invalide.
+ */
+exports.getProfile = (req, res) => {
+    // req.user contient les infos décodées du token JWT
+	res.json({
+		message: 'Voici ton profil',
+		user: req.user
+	});
+};
